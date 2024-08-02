9451071e5f47161417c4cc93d831949753c7b9c7
Casey Mulcahy <caseymulcahy@proton.me>
2024-08-02 18:49:07 -0400

    Move probe defs to separate source file

--- main.go
+++ main.go
@@ -21,7 +21,6 @@ import (
 	"flag"
 	"fmt"
 	"math"
-	"math/rand"
 	"os"
 	"os/exec"
 	"strings"
@@ -31,8 +30,6 @@ import (
 	"github.com/veandco/go-sdl2/ttf"
 	"golang.org/x/exp/maps"
 )
-
-type probe func(string, chan []float64) error
 
 var (
 	interval           = 1 * time.Second
@@ -56,45 +53,6 @@ var (
 
 	//go:embed fonts/noto.ttf
 	fontData []byte
-
-	// for loops without a ticker use more CPU than for loops with a ticker,
-	// even if channel writes are blocking and the channel is only read on the
-	// update interval.
-	probes = map[string]probe{
-		// Basic test probe
-		"sine": func(target string, c chan []float64) error {
-			go func() {
-				i := 0.0
-				for range time.Tick(interval) {
-					c <- []float64{math.Sin(i) + 1}
-					i += 0.1
-				}
-			}()
-			return nil
-		},
-		// Timeout test probe
-		"lagsine": func(target string, c chan []float64) error {
-			go func() {
-				i := 0.0
-				for range time.Tick(interval * 5) {
-					c <- []float64{math.Sin(i) + 1}
-					i += 0.1
-				}
-			}()
-			return nil
-		},
-		// Multi-plot test probe
-		"multi": func(target string, c chan []float64) error {
-			go func() {
-				i := 0.0
-				for range time.Tick(interval) {
-					c <- []float64{math.Sin(i) + 1, math.Cos(i) + 1, rand.Float64() * 2}
-					i += 0.1
-				}
-			}()
-			return nil
-		},
-	}
 )
 
 func errbox(format string, args ...interface{}) {
