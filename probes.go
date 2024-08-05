f0da175d315e446d36f9ef8e56186037b429e11d
Casey Mulcahy <caseymulcahy@proton.me>
2024-08-05 10:49:12 -0400

    Don't use ticker for probes

--- probes.go
+++ probes.go
@@ -8,16 +8,12 @@ import (
 
 type probe func(string, chan []float64) error
 
-// Loops without a ticker use slightly more CPU than for loops with a ticker,
-// even if channel writes are blocking and the channel is only read on the
-// update interval.
-
 var probes = map[string]probe{
 	// Basic test probe
 	"sine": func(target string, c chan []float64) error {
 		go func() {
 			i := 0.0
-			for range time.Tick(interval) {
+			for {
 				c <- []float64{math.Sin(i) + 1}
 				i += 0.1
 			}
@@ -39,7 +35,7 @@ var probes = map[string]probe{
 	"multi": func(target string, c chan []float64) error {
 		go func() {
 			i := 0.0
-			for range time.Tick(interval) {
+			for {
 				c <- []float64{math.Sin(i) + 1, math.Cos(i) + 1, rand.Float64() * 2}
 				i += 0.1
 			}
